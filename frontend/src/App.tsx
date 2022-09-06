5727af4f4507eed57f087c12d45b4aaeee9d8a30
Pedro Arns Junior <pedroarnsjr@outlook.com>
2022-09-06 14:25:27 -0300

    First component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,13 @@
-import React from 'react';
+import NotificationButton from "./components/NotificationButton";
 
-function App() {
-    return(
-    <h1>Olá mundo</h1>
+function App(){
+  return(
+    <>
+      <h1>Olá mundo!</h1>
+      <NotificationButton/>
+      <NotificationButton/>
+      <NotificationButton/>
+    </>
   );
 }
 
